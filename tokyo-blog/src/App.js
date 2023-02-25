a2ea581761cd88ea2408d32978e4d28167bc876e
David Flanik <daveflanik@gmail.com>
2023-02-25 16:21:53 +0900

    added react router and routes

--- tokyo-blog/src/App.js
+++ tokyo-blog/src/App.js
@@ -1,11 +1,23 @@
-
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <nav>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/login'}>Login</Link>
+        <Link to={'/createpost'}>Create Post</Link>
+      </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/createpost" element={<CreatePost />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
   );
 }
 
