2753fdc904bc52d76e81e325f986dcf85db27fe2
David Flanik <daveflanik@gmail.com>
2023-02-27 23:33:08 +0900

    added google pop up sign in.. Issue with removing login nav link

--- tokyo-blog/src/App.js
+++ tokyo-blog/src/App.js
@@ -3,19 +3,21 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
+import { useState } from "react";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false)
   return (
     <Router>
       <nav className='hero-nav'>
         <Link to={'/'}>Home</Link>
         <Link to={'/createpost'}>Create Post</Link>
-        <Link to={'/login'}>Login</Link>
+        {!isAuth ? <Link to={"/login"}>Login</Link> : <button>Log Out</button>}
       </nav>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path="/createpost" element={<CreatePost />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login  setIsAuth={setIsAuth}/>}/>
         </Routes>
       </Router>
   );
