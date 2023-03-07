1aee71a75fa0e130f48f756e4f6c860c1ef58ced
David Flanik <daveflanik@gmail.com>
2023-03-07 21:35:24 +0900

    fix the logout button layout

--- tokyo-blog/src/pages/Login.js
+++ tokyo-blog/src/pages/Login.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import {auth, provider} from "../firebase-config";
+import React from "react";
+import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-function Login({setIsAuth}) {
+function Login({ setIsAuth }) {
   let navigate = useNavigate();
 
   const signInWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
-      localStorage.setItem("isAuth", true)
+      localStorage.setItem("isAuth", true);
       setIsAuth(true);
       navigate("/");
     });
   };
 
   return (
-    <div className='loginPage'>
+    <div className="loginPage">
       <p>Sign In With Google to Continue</p>
-      <button className='login-with-google-btn' onClick={signInWithGoogle}>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
         Sign in with Google
       </button>
     </div>
-  )
+  );
 }
+// need to fix the logout button
 
-export default Login
+export default Login;
